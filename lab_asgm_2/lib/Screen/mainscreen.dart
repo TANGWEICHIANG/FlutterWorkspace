95d1a5c5b8b3b215de9c663d06678793895e0d66
TANGWEICHIANG <92418513+TANGWEICHIANG@users.noreply.github.com>
2023-05-19 18:45:22 +0800

    Add files via upload

---
+++ lab_asgm_2/lib/Screen/mainscreen.dart
@@ -0,0 +1,23 @@
+import 'package:barterit_asgm2/user.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class MainScreen extends StatefulWidget {
+  final User user;
+  const MainScreen({super.key, required this.user});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Main Screen"),
+      ),
+    );
+  }
+}
---
+++ lab_asgm_2/lib/config.dart
@@ -0,0 +1,3 @@
+class Config {
+  String server = "http://10.144.155.193";
+}
